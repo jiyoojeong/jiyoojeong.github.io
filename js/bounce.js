2a8162fc9d20fbfe4a9a231d2d648ae574747306
Jiyoo Jeong <jiyooj@gmail.com>
2020-02-14 23:59:38 -0600

    software2.html is updated! new format. Need to edit content.

---
+++ js/bounce.js
@@ -0,0 +1,8 @@
+function show_all() {
+  var x = document.getElementById("hidden-panel");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
