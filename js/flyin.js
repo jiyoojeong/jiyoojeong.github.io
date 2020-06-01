8e7cc0f930424ab392df30281dfdda470a29d5ce
Jiyoo Jeong <jiyooj@gmail.com>
2020-06-01 01:31:03 -0700

    nav bar edits and flyin.js was fixed

--- js/flyin.js
+++ js/flyin.js
@@ -1,28 +1,29 @@
-
-if (!!document.getElementById("flyin")) {
+function loadflying() {
+    if (!!document.getElementById("flyin")) {
     document.getElementById("flyin").classList.add("run");
-}
-if (!!document.getElementById("flyin2")) {
-    document.getElementById("flyin2").classList.add("run");
-}
+    }
+    if (!!document.getElementById("flyin2")) {
+        document.getElementById("flyin2").classList.add("run");
+    }
 
-if (!!document.getElementById("flyin3")) {
-    document.getElementById("flyin3").classList.add("run");
-}
+    if (!!document.getElementById("flyin3")) {
+        document.getElementById("flyin3").classList.add("run");
+    }
 
-profile_path = ".section > .row > .col-sm-6"
-if (!! document.querySelector(profile_path)) {
-    document.querySelector(profile_path).classList.add("run");
-}
+    profile_path = ".section > .row > .col-sm-6"
+    if (!! document.querySelector(profile_path)) {
+        document.querySelector(profile_path).classList.add("run");
+    }
 
-if (!! document.getElementById("profile")) {
-    document.getElementById("profile").classList.add("run");
-}
+    if (!! document.getElementById("profile")) {
+        document.getElementById("profile").classList.add("run");
+    }
 
-if (!! document.getElementById("headfly")) {
-    document.getElementById("headfly").classList.add("run");
-}
+    if (!! document.getElementById("headfly")) {
+        document.getElementById("headfly").classList.add("run");
+    }
 
-if (!! document.getElementById("section1fly")) {
-    document.getElementById("section1fly").classList.add("run");
+    if (!! document.getElementById("section1fly")) {
+        document.getElementById("section1fly").classList.add("run");
+    }
 }
