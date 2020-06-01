7c695e857d40eff5f634703c549e7b220f1ceb03
Jiyoo Jeong <jiyooj@gmail.com>
2020-05-31 21:05:25 -0700

    flyin adjustments

---
+++ js/flyin.js
@@ -0,0 +1,6 @@
+
+document.getElementById("flyin").className = "run";
+document.getElementById("flyin2").className = "run";
+document.getElementById("flyin3").className = "run";
+document.getElementById("profile").className = "run";
+document.getElementById("header").className = "run";
