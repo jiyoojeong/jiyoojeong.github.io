1193020661817112959f5908cf945becfbaf7735
Jiyoo Jeong <jiyooj@gmail.com>
2020-06-01 02:11:25 -0700

    index.html javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function loadmain() {
+    if (!!document.getElementById("main-name")) {
+    document.getElementById("main-name").classList.add("run");
+    }
+
+    if (!!document.getElementById("main-bar")) {
+        document.getElementById("main-bar").classList.add("run");
+    }
+}
