8d1e5c78e781845303c4c546362aaeed96d1abf9
Jiyoo Jeong <jiyooj@gmail.com>
2020-06-01 01:15:35 -0700

    fixed flyins

--- js/flyin.js
+++ js/flyin.js
@@ -1,6 +1,24 @@
 
-document.getElementById("flyin").className = "run";
-document.getElementById("flyin2").className = "run";
-document.getElementById("flyin3").className = "run";
-document.getElementById("profile").className = "run";
-document.getElementById("header").className = "run";
+if (!!document.getElementById("flyin")) {
+    document.getElementById("flyin").classList.add("run");
+}
+if (!!document.getElementById("flyin2")) {
+    document.getElementById("flyin2").classList.add("run");
+}
+
+if (!!document.getElementById("flyin3")) {
+    document.getElementById("flyin3").classList.add("run");
+}
+
+profile_path = ".section > .row > .col-sm-6"
+if (!! document.querySelector(profile_path)) {
+    document.querySelector(profile_path).classList.add("run");
+}
+
+if (!! document.getElementById("headfly")) {
+    document.getElementById("headfly").classList.add("run");
+}
+
+if (!! document.getElementById("section1fly")) {
+    document.getElementById("section1fly").classList.add("run");
+}
